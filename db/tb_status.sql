3fc233d4a4fb3cba24869a29f85aa1e109a7c92e
Vega <125098430+veganico98@users.noreply.github.com>
2025-04-04 20:49:34 -0300

    Delete db directory

--- db/tb_status.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO php_com_pdo.tb_status (status) VALUES
-	 ('pendente'),
-	 ('realizado');
--- db/tb_tarefas.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO php_com_pdo.tb_tarefas (id_status,tarefa,data_cadastrado) VALUES
-	 (1,'Fazer exercícios','2025-03-11 21:21:10'),
-	 (0,'Pagar a fatura','2025-03-11 21:21:21');
